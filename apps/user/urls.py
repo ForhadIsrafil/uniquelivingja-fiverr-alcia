27eafcee61d242d9dfd88b133062a458fc1ef898
shuvro baset <md.abdul.baset75@gmail.com>
2021-11-10 01:19:38 +0600

    login register done

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,5 +4,6 @@ from . import views
 name = 'user'
 urlpatterns = [
     path("login/", views.login_view, name="login"),
+    path("register/", views.register, name="register"),
     path("logout/", views.logout, name="logout"),
 ]
