f08803c880877769a19bc60432c45414995106c3
forhadisrafil <forhadtuhin5088@gmail.com>
2021-11-18 02:57:35 +0600

    user done

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,4 @@
+from .forms import SignupForm
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
@@ -13,39 +14,69 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import UniUser as User
 
+
 def login_view(request):
-    # if request.user.is_authenticated:
-    #     return redirect('store')
-    # elif request.method == 'POST':
-    #     username = request.POST["username"]
-    #     password = request.POST["password"]
-    #     url = request.POST["url"]
-    #
-    #     user = authenticate(request, username=username, password=password)
-    #     # user = User.objects.get(username=username)
-    #     if user is not None:
-    #     # if user.check_password(raw_password=password):
-    #         login(request, user)
-    #         try:
-    #             next_url = url.split('=')[1]
-    #             return HttpResponseRedirect(next_url)
-    #         except Exception as e:
-    #             return redirect('store')
-    #     else:
-    #         messages.error(
-    #             request,
-    #             "Please use correct credential to login.",
-    #         )
-    #         return render(request, 'users/login.html')
-    # else:
-    return render(request, 'users/login.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    elif request.method == 'POST':
+        username = request.POST["email"]
+        password = request.POST["password"]
+        # url = request.POST["url"]
+
+        user = authenticate(request, username=username, password=password)
+        # user = User.objects.get(username=username)
+        if user is not None:
+            # if user.check_password(raw_password=password):
+            login(request, user)
+            try:
+                # next_url = url.split('=')[1]
+                # return HttpResponseRedirect(next_url)
+                return HttpResponseRedirect('/')
+            except Exception as e:
+                return redirect('home')
+        else:
+            messages.error(
+                request,
+                "Please use correct credential to login.",
+            )
+            return render(request, 'users/login.html')
+    else:
+        return render(request, 'users/login.html')
+
 
 @login_required
 def logout(request):
     # logout(request)
     request.session.flush()
-    return redirect('login')
+    return redirect('home')
 
 
 def register(request):
-    return render(request, 'users/registration.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == "POST":
+            if not User.objects.filter(username=request.POST.get('email')).exists():
+                form = SignupForm(request.POST)
+                if form.is_valid():
+                    user = form.save(commit=False)
+                    user.is_active = True
+                    user.set_password(form.cleaned_data["password"])
+                    user.username = form.cleaned_data["email"]
+                    user.save()
+                    messages.success(request, "Registation successfully completed! Please login.", )
+                    return redirect('login')
+                else:
+                    errors = ''
+                    for field in form:
+                        for error in field.errors:
+                            errors += '| '+field.name.replace('_', ' ').title() + ' | '
+                    messages.error(
+                        request, 'Required Fields: ' + errors)
+                    return render(request, 'users/registration.html')
+            else:
+                messages.error(
+                    request, "Please use correct email. This user already exsit!", )
+                return render(request, 'users/registration.html')
+        else:
+            return render(request, 'users/registration.html')
